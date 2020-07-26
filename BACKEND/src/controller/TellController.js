9bfc37db0c7e9b5b4354f4fcebb50090ee8abe9c
matheus <mvieirinhaa@gmail.com>
2020-07-25 22:29:48 -0300

    criando arquivo de controle das funcoes da aplicacao

---
+++ BACKEND/src/controller/TellController.js
@@ -0,0 +1,19 @@
+const TellSchema = require('../model/TellModel.js')
+
+class TellController {
+
+    async calc(req, res) {
+        const tell = TellSchema(req.body)
+        await tell
+            .save()
+            .then(res => {
+                return res.status(200).json(res)
+            })
+            .catch(error => {
+                return res.status(500).json({error: "algo deu errado"})
+            })
+    }
+
+}
+
+module.exports = new TellController()
